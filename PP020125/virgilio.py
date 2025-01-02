be4dac538ac7e69db055abc573e49f94b72b5574
GiovanniUrso <121301413+GiovanniUrso@users.noreply.github.com>
2025-01-02 18:29:03 +0100

    Code optimized and comm addedd

--- PP020125/virgilio.py
+++ PP020125/virgilio.py
@@ -1,13 +1,12 @@
 # Imported os library to work with files
 
 import os
-# Imported math library to work with numbers
+# Imported math library to work with math operations
 import math
-
-# Created a class called Virgilio
-
+# Imported json library to work with json
 import json
 
+# Created a class called Virgilio
 
 class Virgilio:
     # Created a constructor as self and directory as parameters
@@ -17,20 +16,23 @@ class Virgilio:
     # Created a method called read_canto_lines for reading the canto from file and to format it and return the lines
     def read_canto_lines(self,canto_number):
         
+        # Created a class called CantoNotFoundError extends Exception for a custom error managment
         class CantoNotFoundError(Exception):
             def __init__(self, message):
                super().__init__(message)
               
         
-        
+        # Verify if canto_number is an integer
         
         if not (isinstance(canto_number,int)):
             raise TypeError("canto_number must be an integer")
+        
+        # Verify if canto_number is between 1 and 34 for raise exeption
         if canto_number < 1 or canto_number > 34:
-        
             raise CantoNotFoundError("canto_number must be between 1 and 34")
+        # Declare a variable called strip_lines that start with False
         strip_lines = False
-        
+        # Declare a variable called num_lines that start with None
         num_lines=None
       # Created a variable called file_path for storing the path of the file
         file_path = os.path.join(self.directory, f"Canto_{canto_number}.txt")
@@ -43,9 +45,11 @@ class Virgilio:
                 # Print the lines only for debugging and testing
                 #print(lines)
                 # Return the lines
+            # Verify if strip_lines is True and make the lines strip for all line using for cicle
             if strip_lines:
                  lines = [line.strip() for line in lines]
                  return lines
+            # Verify if num_lines is not None and take the number of lines defined on num_lines value
             if num_lines is not None:
                  lines = file.readlines(num_lines)
                  return lines
@@ -54,12 +58,12 @@ class Virgilio:
                 
         except FileNotFoundError:
             # Print a message if the file is not found or if number of canto file is not found
-            print(f"File not found: {file_path}")
-            return None
+            
+            return print(f"File not found: {file_path}")
             # Print a message if there is an generic error 
         except Exception as e:
-            print(f"An error occurred: {e}")
-            return None
+            
+            return print(f"An error occurred: {e}")
         
         
         
@@ -73,55 +77,69 @@ class Virgilio:
     def count_tercets(self,canto_number):
         # Recall last method for do this and get the number of verses for divide by 3
         canto_number = self.count_verses(canto_number)/3
-        # Return the number of terzine
+        # Return the number of terzine around using floor method of math library
         return math.floor(canto_number)
+    # Created a method called count_word for counting the number of word in canto
     def  count_word(self,canto_number,word):
-        
-        file_path = os.path.join(self.directory, f"Canto_{canto_number}.txt")
-        with open(file_path, "r", encoding="utf-8") as file:
-            content = file.read()
-            return content.count(word)
-        
+            # Recall last method for do this and get the number of lines for count the number of word in that       
+            lines = self.read_canto_lines(canto_number)
+            # Return the count of word in lines
+            return lines.content.count(word)
+    # Created a method called get_verse_whit_word for counting the number of word in canto    
     def get_verse_whit_word(self,canto_number,word):
+            # Define an empty list that will store the lines
             line_list = []
-            file_path = os.path.join(self.directory, f"Canto_{canto_number}.txt")
-            with open(file_path, "r", encoding="utf-8") as file:
-                lines = file.readlines()
-                for line in lines:
+            # Recall submethod of my first method for get lines
+            lines = self.read_canto_lines(canto_number).readlines()
+            # Cicle for each line and if the word is in the line, add the line to the list
+            for line in lines:
                     if word in line:
                         line_list.append(line)
-                return line_list
-        
+                        # Return the full list
+                        return line_list
+    # Defined a method called get_longest_verse for get the longest verse    
     def  get_longest_verse(self,canto_number):
-        file_path = os.path.join(self.directory, f"Canto_{canto_number}.txt")
-        with open(file_path, "r", encoding="utf-8") as file:
-            lines = file.readlines()
-            longest_line = max(lines, key=len)
-            return longest_line
+        lines = self.read_canto_lines(canto_number).readlines()  # must be fixed
+        longest_line = max(lines, key=len)
+        return longest_line
+    # Defined a method called get_longest_canto for get the longest canto
     def get_longest_canto(self):
+        # Define a dictionary that will store the longest canto
         longest_canto = {"canto_number": None, "canto_len": 0}
+        # Cicle for each canto in range of 1 to 34
         for canto_number in range(1, 34):
-            file_path = os.path.join(self.directory, f"Canto_{canto_number}.txt")
-            with open(file_path, "r", encoding="utf-8") as file:
-                lines = file.readlines()
-                canto_len = sum(len(line) for line in lines)
-                if canto_len > longest_canto["canto_len"]:
+            # Recall submethod of my first method for get lines
+            lines = self.read_canto_lines(canto_number).readlines()
+            # Calculate the length of the canto
+            canto_len = sum(len(line) for line in lines)
+            # If the length of the canto is greater than the current longest canto, update the longest canto
+            if canto_len > longest_canto["canto_len"]:
+                    # Update the longest canto with the current canto key
                     longest_canto["canto_number"] = canto_number
+                    # Update the longest canto with the current canto value
                     longest_canto["canto_len"] = canto_len
         return longest_canto
-    
+    # Defined a method called count_words for count the number of word and write in json file
     def count_words(self,canto_number,word):
+        # Define json file name
         json_file_name = "word_counts.json"
+        # Define json file path and use os library to work with files
         json_file_path = os.path.join(os.getcwd(), json_file_name)
+        # Define a dictionary that will store the word counts start with 0
         words_in_canto = {word: 0}
+        # Recall submethod of my first method for get lines
         file_path = os.path.join(self.directory, f"Canto_{canto_number}.txt")
         with open(file_path, "r", encoding="utf-8") as file:
+            # Read the content of the file
             content = file.read()
+            # Cicle for each word in the content
             for word in content:
+                # If the word is in the dictionary, add 1 to the count can do also words_in_canto[word] += 1
                 words_in_canto[word] = content.count(word)
                 
-                
+        # Open the json file        
         with open (json_file_path, "w") as json_file:
+            # Write the dictionary in the json file using json library and setting parameters
             json_file = json.dump(words_in_canto, json_file, indent=4, ensure_ascii=False)
             
                 
