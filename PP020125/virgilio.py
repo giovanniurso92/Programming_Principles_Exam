aeb427e917ed81257175f819ae64c8a8a66bcdb6
GiovanniUrso <121301413+GiovanniUrso@users.noreply.github.com>
2025-01-03 22:08:43 +0100

    Debugging 3

--- PP020125/virgilio.py
+++ PP020125/virgilio.py
@@ -28,10 +28,7 @@ class Virgilio:
         # Verify if canto_number is between 1 and 34 for raise exeption
         if canto_number < 1 or canto_number > 34:
             raise CantoNotFoundError("canto_number must be between 1 and 34")
-        # Declare a variable called strip_lines that start with False
-        strip_lines = False
-        # Declare a variable called num_lines that start with None
-        num_lines=None
+       
       # Created a variable called file_path for storing the path of the file
         file_path = os.path.join(self.directory, f"Canto_{canto_number}.txt")
       # Use try-except block for error handling and read the file
@@ -45,17 +42,19 @@ class Virgilio:
                 # Return the lines
             # Verify if strip_lines is True and make the lines strip for all line using for cicle
                 if strip_lines:
+                    strip_lines = []
                     for line in lines:
-                        line.strip()
+                        strip_line = line.strip()
+                        strip_lines.append(strip_line)
                     # Return the lines
-                    return lines
+                    return strip_lines
                     
                 # Verify if num_lines is not None and take the number of lines defined on num_lines value
                 if num_lines is not None:
                     
                     lines = [lines for _ in range(num_lines)]
                     return lines
-            return lines    
+                return lines    
                 
         except FileNotFoundError:
             # Print a message if the file is not found or if number of canto file is not found
@@ -84,20 +83,17 @@ class Virgilio:
     def  count_word(self,canto_number,word):
             # Recall last method for do this and get the number of lines for count the number of word in that       
             lines = self.read_canto_lines(canto_number)
+            # Take all lines as one string 
+            all_word_of_canto = "".join(lines)
+            # Return the count of word in canto
+            return all_word_of_canto.count(word)
             # Return the count of word in lines
-            return lines.content.count(word)
-    # Created a method called get_verse_whit_word for counting the number of word in canto    
-    def get_verse_whit_word(self,canto_number,word):
+               # Created a method called get_verse_whit_word for counting the number of word in canto    
+    def get_verse_with_word(self,canto_number,word):
             # Define an empty list that will store the lines
-            line_list = []
-            # Recall submethod of my first method for get lines
             lines = self.read_canto_lines(canto_number)
-            # Cicle for each line and if the word is in the line, add the line to the list
-            for line in lines:
-                    if word in line:
-                        line_list.extend(line)
-                        # Return the full list
-                        return line_list
+            # Return the first line that contains the word
+            return next((line for line in lines if word in line), None)
     # Defined a method called get_longest_verse for get the longest verse    
     def  get_longest_verse(self,canto_number):
         # Recall submethod of my first method for get lines
@@ -112,10 +108,8 @@ class Virgilio:
         longest_canto = {"canto_number": None, "canto_len": 0}
         # Cicle for each canto in range of 1 to 34
         for canto_number in range(1, 34):
-            # Recall submethod of my first method for get lines
-            lines = self.read_canto_lines(canto_number).readlines()
-            # Calculate the length of the canto
-            canto_len = sum(len(line) for line in lines)
+            # Calculate the length of the canto using method created before
+            canto_len = self.count_verses(canto_number)
             # If the length of the canto is greater than the current longest canto, update the longest canto
             if canto_len > longest_canto["canto_len"]:
                     # Update the longest canto with the current canto key
@@ -124,23 +118,21 @@ class Virgilio:
                     longest_canto["canto_len"] = canto_len
         return longest_canto
     # Defined a method called count_words for count the number of word and write in json file
-    def count_words(self,canto_number,word):
+    def count_words(self,canto_number,words):
         # Define json file name
         json_file_name = "word_counts.json"
         # Define json file path and use os library to work with files
         json_file_path = os.path.join(os.getcwd(), json_file_name)
-        # Define a dictionary that will store the word counts start with 0
-        words_in_canto = {word: 0}
-        # Recall submethod of my first method for get lines
-        file_path = os.path.join(self.directory, f"Canto_{canto_number}.txt")
-        with open(file_path, "r", encoding="utf-8") as file:
-            # Read the content of the file
-            content = file.read()
-            # Cicle for each word in the content
-            for word in content:
-                # If the word is in the dictionary, add 1 to the count can do also words_in_canto[word] += 1
-                words_in_canto[word] = content.count(word)
-                
+        # Define a dictionary that will store the word counts start with empty
+         
+        words_in_canto = {}
+        # Cicle for each word in words
+          
+        for word in words:
+            # Add the word to the dictionary
+           
+            words_in_canto[word] = self.count_word(canto_number,word)   
+                       
         # Open the json file        
         with open (json_file_path, "w") as json_file:
             # Write the dictionary in the json file using json library and setting parameters
@@ -178,5 +170,13 @@ class Virgilio:
         return  float(mean_len)
     
 virgilio = Virgilio("./PP020125/canti")
-print(virgilio.get_hell_verses())
+
+# print(virgilio.read_canto_lines(1,strip_lines=True))
+print(virgilio.count_hell_verses())
+# print(virgilio.read_canto_lines(1,strip_lines=True))
+# print(virgilio.read_canto_lines(1,strip_lines=True))
+# print(virgilio.read_canto_lines(1,strip_lines=True))
+# print(virgilio.read_canto_lines(1,strip_lines=True))
+# print(virgilio.read_canto_lines(1,strip_lines=True))
+
 
